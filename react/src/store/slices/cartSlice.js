91d546ad71b63576a8b59e4233c95ec408f65abc
dorokhin-admin <dorokhin-ilyaa@gmail.com>
2026-04-16 21:42:03 +0500

    синхронизировал работу функции плюс минус и удаление с добавлением товаром с сервером

--- react/src/store/slices/cartSlice.js
+++ react/src/store/slices/cartSlice.js
@@ -25,9 +25,9 @@ const cartSlice = (set, get) => ({
         }));
     },
 
-    removeFromCart: async (productId) => {
+    removeFromCart: async (id) => {
         const state = get();
-        const order = state.orders.find(o => o.productId === productId);
+        const order = state.orders.find(o => o.id === id);
         if (!order) return;
 
         await fetch(`http://localhost:3001/orders/${order.id}`, {
@@ -35,29 +35,64 @@ const cartSlice = (set, get) => ({
         });
 
         set((state) => ({
-            orders: state.orders.filter(o => o.productId !== productId)
+            orders: state.orders.filter(o => o.id !== order.id)
         }));
     },
 
-    plus: (id) =>
-        set((state) => ({
-            orders: state.orders.map(o =>
-                o.id === id
-                    ? { ...o, quantity: o.quantity + 1 }
-                    : o
-            )
-        })),
-
-    minus: (id) => {
+    plus: (id) => {
         set((state) =>(
             { orders: state.orders
                     .map(order => order.id === id
-                        ? {...order, quantity: Math.max(order.quantity - 1, 0)}
-                        : order)
-                    .filter(order => order.quantity > 0)
+                        ? {...order, quantity: order.quantity + 1}
+                        : order
+                    )
             }
         ))
     },
+
+        minus: async (id) => {
+            const state = get();
+
+            const order = state.orders.find(o => o.id === id);
+            if (!order) return;
+
+            const newQuantity = order.quantity - 1;
+
+            // 1. если стало 0 — удалить
+            if (newQuantity <= 0) {
+                await fetch(`http://localhost:3001/orders/${order.id}`, {
+                    method: 'DELETE',
+                });
+
+            set((state) => ({
+                orders: state.orders.filter(o => o.id !== id)
+            }))
+                return
+            }
+
+            // 2. если больше 0 — обновить на сервере
+            await fetch(`http://localhost:3001/orders/${order.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    quantity: newQuantity
+                })
+            });
+
+            set((state) => ({
+                orders: state.orders.map(o =>
+                    o.id === id
+                    ? { ...o, quantity: newQuantity}
+                    : o
+                )
+            }))
+        },
+
+    //item.id - в каталоге
+    //productId - в корзине
+    //order.id - на сервере
 
     toggleSelect: (id) => {
         set((state) => {
